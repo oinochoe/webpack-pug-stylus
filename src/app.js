33c32064369e58d242f8bea1e4e8a5525a3f3b56
yeongminkim <oinochoe@naver.com>
2020-04-18 16:22:38 +0900

    svg path stylus2 write

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-import css from './style.styl';
+import css from './style2.styl';
+window.addEventListener('DOMContentLoaded', () => {
+    const INPUT = document.querySelector('input');
+    const BLOCK = document.querySelector('.block');
+    const H = document.querySelector('h1');
+
+    const update = (e) => {
+        BLOCK.style.setProperty('--distance', e.target.value);
+        H.innerHTML = `offset-distance: ${e.target.value}%;`;
+    };
+
+    INPUT.addEventListener('input', update);
+});

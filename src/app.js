3c8c30b30f5468ceb5b41bedb03af28b68cfb93c
yeongminkim <oinochoe@naver.com>
2020-04-18 16:45:17 +0900

    update functionality

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import css from './style2.styl';
 window.addEventListener('DOMContentLoaded', () => {
-    const INPUT = document.querySelector('input');
+    const DISTANCE = document.querySelector('input.distance');
+    const ROTATE = document.querySelector('input.rotate');
     const BLOCK = document.querySelector('.block');
-    const H = document.querySelector('h1');
+    const HD = document.querySelector('h1.distance');
+    const HR = document.querySelector('h1.rotate');
 
-    const update = (e) => {
+    const updateDistance = (e) => {
         BLOCK.style.setProperty('--distance', e.target.value);
-        H.innerHTML = `offset-distance: ${e.target.value}%;`;
+        HD.innerHTML = `offset-distance: ${e.target.value}%;`;
     };
 
-    INPUT.addEventListener('input', update);
+    const updateRotate = (e) => {
+        BLOCK.style.setProperty('--rotate', e.target.value);
+        HR.innerHTML = `offset-rotate: ${e.target.value}deg;`;
+    };
+
+    ROTATE.addEventListener('input', updateRotate);
+    DISTANCE.addEventListener('input', updateDistance);
 });
